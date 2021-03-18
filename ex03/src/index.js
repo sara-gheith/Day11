8673531ce7218d037d72b5a31f0ad127def3e5dc
sara-gheith <saraannegheith@gmail.com>
2021-03-19 00:58:39 +0100

    Manipulate Arrays

---
+++ ex03/src/index.js
@@ -0,0 +1,16 @@
+myPetsArray = ["Dog", "Cat"];
+
+function myArrayFunction(myPets) {
+    var myNewPets = [...myPets];
+    myNewPets = myPetsArray;
+
+    myNewPets.push("Bird", "Fish");
+    var firstPet = myNewPets.shift();
+    var lastPet = myNewPets.pop();
+    myNewPets.unshift("Lion");
+
+    return myNewPets;
+}
+
+console.log(myArrayFunction(myPetsArray));
+module.exports = myArrayFunction;

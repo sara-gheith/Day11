905e60e23ca76735caf59e12444a42872242d68f
sara-gheith <saraannegheith@gmail.com>
2021-03-19 04:29:51 +0100

    Nesting For Loops

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+function multiplyArrayFunction(myArray) {
+    arr = [...myArray];
+    var product = [
+        [[2], [5, 6, 7], [8, 9]],
+        [[2.5], [0.5, 0.2], [8]],
+        [[1, 2], [3, 4, 5, 6], [7, 8, 9]]
+    ];
+    var sum;
+}
+
+console.log(multiplyArrayFunction(product));
+module.exports = multiplyArrayFunction;
